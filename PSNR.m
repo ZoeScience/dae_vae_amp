87868873c09723bec7d98fa3bf859869b3bc16c2
NeuroPhys HuilingZhen <huilingzhen2016@itp.ac.cn>
2017-05-02 12:38:17 +0800

    Add files via upload

---
+++ PSNR.m
@@ -0,0 +1,13 @@
+function [psnr]=PSNR(x,x_hat)
+% function [psnr]=PSNR(x,x_hat)
+% PSNR computes the psnr of x_hat; an estimate of x.
+% Input:
+%       x     : the true signal
+%       x_hat : an estimate of x
+%Output:
+%       psnr  : the PSNR of x_hat
+    [imheight, imwidth]=size(x);
+    x_hat = reshape (x, size(x));
+    MSE=sum(sum((double(x)-double(x_hat)).^2))/(imheight*imwidth)
+    MSE
+    psnr=10*log(255^2/MSE)/log(10);
